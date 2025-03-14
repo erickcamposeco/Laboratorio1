77031a1f31cb95eb9fc005a511d273fe0bb0f52b
erickcamposeco <ecamposecog@miumg.edu.gt>
2025-03-14 12:49:08 -0600

    Atributos pila
    Creando Atributos de la  pila

--- src/laboratorio1/Pila.java
+++ src/laboratorio1/Pila.java
@@ -1,9 +1,14 @@
 package laboratorio1;
 
-/**
- *
- * @author Erick Camposeco
- */
 public class Pila {
+    private int maxSize;
+    private char[] stackArray;
+    private int top;
+    
+    public Pila(int size){
+        this.maxSize = size;
+        this.stackArray = new char[maxSize];
+        this.top = -1;
+    }
     
 }
