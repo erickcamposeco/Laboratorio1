267c243be4849dfe4bd330d40336fee98841cd71
erickcamposeco <ecamposecog@miumg.edu.gt>
2025-03-14 12:59:01 -0600

    Clase InfijoPosfijoPrefijo
    creacion de clase InfijoPosfijoPrefijo

---
+++ src/laboratorio1/InfijoPosfijoPrefijo.java
@@ -0,0 +1,66 @@
+
+package laboratorio1;
+
+
+public class InfijoPosfijoPrefijo {
+    
+    // Método para verificar si un carácter es un operador
+    public static boolean isOperador(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+    }
+
+    // Método para invertir una cadena
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+    
+    // Método para validar la jerarquía de los operadores
+    public static int validateHierarchy(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    // Método para convertir de infijo a postfijo
+    public static String convertToPostfix(String infijo) {
+        StringBuilder postfijo = new StringBuilder();
+        Pila pila = new Pila(infijo.length());
+
+        for (int i = 0; i < infijo.length(); i++) {
+            char ch = infijo.charAt(i);
+
+            if (Character.isLetterOrDigit(ch)) {
+                postfijo.append(ch);
+            } else if (ch == '(') {
+                pila.push(ch);
+            } else if (ch == ')') {
+                while (!pila.isEmpty() && pila.peek() != '(') {
+                    postfijo.append(pila.pop());
+                }
+                pila.pop(); // Sacar '(' de la pila
+            } else if (isOperador(ch)) {
+                while (!pila.isEmpty() && validateHierarchy(ch) <= validateHierarchy(pila.peek())) {
+                    postfijo.append(pila.pop());
+                }
+                pila.push(ch);
+            }
+        }
+
+        while (!pila.isEmpty()) {
+            postfijo.append(pila.pop());
+        }
+
+        return postfijo.toString();
+    }
+    
+    
+}
+
